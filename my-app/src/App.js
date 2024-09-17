6b91e744afc9f596ba7caaff15ae635684d7208b
ADRjEN5 <znoskoadrian@gmail.com>
2024-09-17 11:17:44 +0200

    Pierwsze zajęcia

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import InfoBox from './compoments/infoBox';
+import List from './compoments/List/List';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <InfoBox/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/compoments/infoBox.jsx
@@ -0,0 +1,9 @@
+
+import React from "react"
+
+const InfoBox = () =>{
+    const name = "Adrian"
+    return <p>Nazywam się {name}</p>
+}
+
+export default InfoBox
